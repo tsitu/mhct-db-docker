dc109044e8a3ed4b2539448e29e6bc9ea334f46a
Tran Situ <trancsitu@gmail.com>
2020-06-25 16:23:43 -0700

    Pretty up load_data.sh

--- load_data.sh
+++ load_data.sh
@@ -3,8 +3,13 @@
 for fullname in /docker-entrypoint-initdb.d/*.txt; do
   filename="${fullname%.*}"
   file="${filename##*/}"
+  echo "----------"
+  date
+  echo "Loading data into table '${file}'"
   time mysql -u admin -padmin mhconverter \
     -e "SET autocommit = 0; SET unique_checks = 0; SET foreign_key_checks = 0;" \
     -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/${file}.txt' INTO TABLE $file;" \
     -e "commit;"
 done
+
+echo "----------"
