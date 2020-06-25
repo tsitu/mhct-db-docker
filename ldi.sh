afe89310336b29b4a9b06d28391fca36781565dc
Tran Situ <trancsitu@gmail.com>
2020-06-25 00:31:05 -0700

    ldi.sh test 4

--- ldi.sh
+++ ldi.sh
@@ -4,8 +4,9 @@
 # sleep 5
 
 # services mysql start
-/etc/init.d/mysql start
+# /etc/init.d/mysql start
 
-mysql -u root -p secret -e "CREATE DATABASE testz"
-mysql -u root -p secret --local_infile=1 -e "USE testz" -e "LOAD DATA INFILE 'converter_weekly.txt.zip'"
-# mysql -u root -p secret LOAD DATA INFILE './converter_weekly.txt.zip' INTO TABLE testz
+mysql -u admin -p admin -e "CREATE DATABASE testz"
+mysql -u admin -p admin -e "set autocommit=0; set unique_checks=0; set foreign_key_checks=0; set sql_log_bin=0;" -e "load data infile './converter_weekly.txt.zip' into testz; commit;"
+# mysql -u admin -p admin --local_infile=1 -e "USE testz" -e "LOAD DATA INFILE 'converter_weekly.txt.zip'"
+# mysql -u admin -p admin LOAD DATA INFILE './converter_weekly.txt.zip' INTO TABLE testz
