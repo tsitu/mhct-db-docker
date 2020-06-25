68debf345d101acdfbb298fde4618afbce91473e
Tran Situ <trancsitu@gmail.com>
2020-06-25 00:46:49 -0700

    ldi.sh test 8

--- ldi.sh
+++ ldi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# /usr/bin/mysqld_safe --skip-grant-tables &
-# sleep 5
+/usr/bin/mysqld_safe --skip-grant-tables &
+sleep 5
 
-mysql -u root -e "set autocommit=0; set unique_checks=0; set foreign_key_checks=0; set sql_log_bin=0;" -e "load data infile './converter_weekly.txt.zip' into mhconverter; commit;"
+mysql -u root -p -e "set autocommit=0; set unique_checks=0; set foreign_key_checks=0; set sql_log_bin=0;" -e "load data infile './converter_weekly.txt.zip' into mhconverter; commit;"
 # mysql -u admin -padmin -e "set autocommit=0; set unique_checks=0; set foreign_key_checks=0; set sql_log_bin=0;" -e "load data infile './converter_weekly.txt.zip' into mhconverter; commit;"
 # mysql -u admin -p admin --local_infile=1 -e "USE testz" -e "LOAD DATA INFILE 'converter_weekly.txt.zip'"
