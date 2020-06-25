530a94e180f7416c8a48b1294c4d5f12ae971970
Tran Situ <trancsitu@gmail.com>
2020-06-25 16:52:49 -0700

    Minor load_data tweaks

--- load_data.sh
+++ load_data.sh
@@ -7,8 +7,8 @@ for fullname in /docker-entrypoint-initdb.d/*.txt; do
   date
   echo "Loading data into table '${file}'"
   time mysql -u admin -padmin mhconverter \
-    -e "SET autocommit = 0; SET unique_checks = 0; SET foreign_key_checks = 0;" \
-    -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/${file}.txt' INTO TABLE $file;" \
+    -e "SET autocommit=0; SET unique_checks=0; SET foreign_key_checks=0;" \
+    -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/${file}.txt' INTO TABLE $file FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';" \
     -e "commit;"
 done
 
