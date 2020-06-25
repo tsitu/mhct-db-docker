b1c8abd4d1043af5bf1355b5dd7c0b5f62aee05e
Tran Situ <trancsitu@gmail.com>
2020-06-25 00:26:13 -0700

    ldi.sh test 3

--- ldi.sh
+++ ldi.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-/usr/bin/mysqld_safe --skip-grant-tables &
-sleep 5
-mysql -u root -e "CREATE DATABASE testz"
-mysql -u root LOAD DATA INFILE './converter_weekly.txt.zip' INTO TABLE testz;
+
+# /usr/bin/mysqld_safe --skip-grant-tables &
+# sleep 5
+
+# services mysql start
+/etc/init.d/mysql start
+
+mysql -u root -p secret -e "CREATE DATABASE testz"
+mysql -u root -p secret --local_infile=1 -e "USE testz" -e "LOAD DATA INFILE 'converter_weekly.txt.zip'"
+# mysql -u root -p secret LOAD DATA INFILE './converter_weekly.txt.zip' INTO TABLE testz
