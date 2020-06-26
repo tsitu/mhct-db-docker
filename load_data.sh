9d362b8383caead2e0c8f4484010b1202f2d0507
Tran Situ <trancsitu@gmail.com>
2020-06-25 22:29:37 -0700

    Retrying with binary logging disabled

--- load_data.sh
+++ load_data.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 mysql -u root -psecret -e "SET GLOBAL innodb_buffer_pool_size=1073741824;"
+mysql -u root -psecret -e "SET sql_log_bin=OFF;"
 
 mysql -u admin -padmin mhhunthelper -e "SET autocommit=0; SET unique_checks=0; SET foreign_key_checks=0;"
 
 for fullname in /docker-entrypoint-initdb.d/*.txt; do
+  echo "----------"
   filename="${fullname%.*}"
   file="${filename##*/}"
-  echo "----------"
   date
   echo "Loading data into table '${file}'"
   time mysql -u admin -padmin mhhunthelper \
     -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/${file}.txt' INTO TABLE $file FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';" \
     -e "COMMIT;"
+  sleep 1
 done
 
 mysql -u admin -padmin mhhunthelper -e "SET autocommit=1; SET unique_checks=1; SET foreign_key_checks=1;"
+mysql -u root -psecret -e "SET sql_log_bin=ON;"
 
 echo "----------"
