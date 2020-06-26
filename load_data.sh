10c03090dda2832fa2c9598c11960a36ba50a1b8
Tran Situ <trancsitu@gmail.com>
2020-06-25 18:16:31 -0700

    Testing InnoDB buffer pool size increase

--- load_data.sh
+++ load_data.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mysql -u root -psecret -e "SET GLOBAL innodb_buffer_pool_size=1073741824;"
+
+mysql -u admin -padmin mhconverter -e "SET autocommit=0; SET unique_checks=0; SET foreign_key_checks=0;"
+
 for fullname in /docker-entrypoint-initdb.d/*.txt; do
   filename="${fullname%.*}"
   file="${filename##*/}"
@@ -7,9 +11,10 @@ for fullname in /docker-entrypoint-initdb.d/*.txt; do
   date
   echo "Loading data into table '${file}'"
   time mysql -u admin -padmin mhconverter \
-    -e "SET autocommit=0; SET unique_checks=0; SET foreign_key_checks=0;" \
     -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/${file}.txt' INTO TABLE $file FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';" \
-    -e "commit;"
+    -e "COMMIT;"
 done
+
+mysql -u admin -padmin mhconverter -e "SET autocommit=1; SET unique_checks=1; SET foreign_key_checks=1;"
 
 echo "----------"
