9518c00a107ea27e697d51c5363d28b18a12aa41
Tran Situ <trancsitu@gmail.com>
2020-06-25 18:20:24 -0700

    Testing buffer pool size increase

--- load_data.sh
+++ load_data.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+mysql -u root -psecret -e "SET GLOBAL innodb_buffer_pool_size=1073741824;"
+
+mysql -u admin -padmin mhhunthelper -e "SET autocommit=0; SET unique_checks=0; SET foreign_key_checks=0;"
+
 for fullname in /docker-entrypoint-initdb.d/*.txt; do
   filename="${fullname%.*}"
   file="${filename##*/}"
+  echo "----------"
+  date
+  echo "Loading data into table '${file}'"
   time mysql -u admin -padmin mhhunthelper \
-    -e "SET autocommit = 0; SET unique_checks = 0; SET foreign_key_checks = 0;" \
-    -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/${file}.txt' INTO TABLE $file;" \
-    -e "commit;"
+    -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/${file}.txt' INTO TABLE $file FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';" \
+    -e "COMMIT;"
 done
+
+mysql -u admin -padmin mhhunthelper -e "SET autocommit=1; SET unique_checks=1; SET foreign_key_checks=1;"
+
+echo "----------"
